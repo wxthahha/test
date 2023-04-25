9da82f667257994537ff0d3eef16c7e7a44bf19c
wangxiaoting <wangxiaoting@4paradigm.com>
2023-04-25 22:24:47 +0800

    docs:ts函数

--- ts/function.ts
+++ ts/function.ts
@@ -2,7 +2,7 @@
  * @Author: wangxiaoting wangxiaoting@4paradigm.com
  * @Date: 2023-04-24 13:18:07
  * @LastEditors: wangxiaoting wangxiaoting@4paradigm.com
- * @LastEditTime: 2023-04-25 21:04:49
+ * @LastEditTime: 2023-04-25 22:12:42
  * @Description: ts函数和class
  */
 function foo(name: string): number {
@@ -35,4 +35,26 @@ function foo1(name: string, age?: number): number {
 // 或如下使用方式
 function foo2(name: string, age: number = 18): number {
   return name.length + age;
+}
+
+//* 函数重载签名
+// function func(foo: number, bar?: boolean): string | number {
+//   if (bar) {
+//     return "aaa";
+//   } else {
+//     return 111;
+//   }
+// }
+// 重载
+// 函数的重载签名一
+function func(foo: number, bar: true): string;
+// 函数的重载签名二
+function func(foo: number, bar?: false): number;
+// 函数的实现签名
+function func(foo: number, bar?: boolean): string | number {
+  if (bar) {
+    return "aaa";
+  } else {
+    return 111;
+  }
 }
