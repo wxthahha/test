ed12c52044acce4443b19e6026e182149ffa4ca1
wangxiaoting <wangxiaoting@4paradigm.com>
2023-05-06 15:47:13 +0800

    feat:ts中的class类型

--- ts/class.ts
+++ ts/class.ts
@@ -2,7 +2,9 @@ class Foo {
   constructor(public a: string, private b: number, protected c: boolean) {}
 }
 
-class FooChild extends Foo {}
+class FooChild extends Foo {
+  aaa: string = super.a;
+}
 
 const _classInst = new Foo("aaa", 111, true);
 
