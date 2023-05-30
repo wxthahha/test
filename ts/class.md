fd58ad5fec2fe2a88cbc818dfb569ec12bf62323
wangxiaoting <wangxiaoting@4paradigm.com>
2023-05-30 13:33:54 +0800

    feat:solid原则

--- ts/class.md
+++ ts/class.md
@@ -19,3 +19,13 @@ public、private、protected
 #### interface 也可以描述类
 
 ### 私有构造函数
+
+```
+class Foo3 {
+  private constructor() {}
+}
+
+// const _foo3 = new Foo3(); //! 类“Foo3”的构造函数是私有的，仅可在类声明中访问
+```
+
+是为了防止类被实例化。
--- ts/class.ts
+++ ts/class.ts
@@ -2,7 +2,7 @@
  * @Author: wangxiaoting wangxiaoting@4paradigm.com
  * @Date: 2023-04-25 23:13:12
  * @LastEditors: wangxiaoting wangxiaoting@4paradigm.com
- * @LastEditTime: 2023-05-22 13:54:15
+ * @LastEditTime: 2023-05-30 13:19:10
  */
 class Foo {
   constructor(public a: string, private b: number, protected c: boolean) {
@@ -24,8 +24,8 @@ class FooChild extends Foo {
 const _classChildInst = new FooChild("bbb", 222, false);
 
 // console.log(_classInst.a);
-console.log(_classChildInst.a);
-console.log(_classChildInst.aaa);
+// console.log(_classChildInst.a);
+// console.log(_classChildInst.aaa);
 _classChildInst.printWithLove();
 
 // 抽象类
@@ -47,7 +47,7 @@ class Foo1 implements AbsFoo {
   static A: number = 111;
 }
 
-console.log(Foo1.A);
+// console.log(Foo1.A);
 
 // interface 描述类
 interface IAbsFoo {
@@ -67,9 +67,15 @@ class Foo2 implements IAbsFoo {
   static B: number = 222;
 }
 
-console.log(Foo2.B);
+// console.log(Foo2.B);
 
 const _foo2 = new Foo2();
-console.log(_foo2.absProps);
-console.log(_foo2.absGetter);
-console.log(_foo2.absMethod("_foo2"));
+// console.log(_foo2.absProps);
+// console.log(_foo2.absGetter);
+// console.log(_foo2.absMethod("_foo2"));
+
+class Foo3 {
+  private constructor() {}
+}
+
+// const _foo3 = new Foo3(); //! 类“Foo3”的构造函数是私有的，仅可在类声明中访问
