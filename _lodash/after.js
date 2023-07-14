3c330d270c7af5b509ffcfefe898b0a7afda243f
wangxiaoting <wangxiaoting@4paradigm.com>
2023-07-14 23:20:00 +0800

    refactor:lodash-after

---
+++ _lodash/after.js
@@ -0,0 +1,13 @@
+const after = (n, func) => {
+  if (typeof func !== "function") {
+    throw TypeError("needed function");
+  }
+  n = n || 0;
+  return function(...args){
+    if (--n < 1) {
+      return func.apply(this, args);
+    }
+  };
+};
+
+export default after;
