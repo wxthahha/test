ad54da008e56bb9e983af02b33e1abd001c6cf94
王晓婷 <wangxiaoting@4paradigm.com>
2023-07-24 17:56:54 +0800

    feat:isObjectLike

--- _lodash/test/isObjectLike.test.js
+++ _lodash/test/isObjectLike.test.js
@@ -2,13 +2,13 @@
  * @Author: 王晓婷 wangxiaoting@4paradigm.com
  * @Date: 2023-07-18 14:34:58
  * @LastEditors: 王晓婷 wangxiaoting@4paradigm.com
- * @LastEditTime: 2023-07-18 18:46:14
+ * @LastEditTime: 2023-07-24 17:56:18
  * @Description: isObjectLike 测试用例
  */
 import assert from "assert";
-import _ from "lodash";
+import lodashStable from "lodash";
 import isObjectLike from "../isObjectLike.js";
-import { args } from "./util.js";
+import { args, falsey, symbol, slice, stubFalse, realm } from "./util.js";
 
 describe("isObjectLike", function () {
   it("should return `true` for objects", function () {
@@ -23,16 +23,22 @@ describe("isObjectLike", function () {
   });
 
   it("should return `false` for non-objects", function () {
-    assert.strictEqual(isObjectLike(1), false);
-    assert.strictEqual(isObjectLike("a"), false);
-    assert.strictEqual(isObjectLike(null), false);
-    assert.strictEqual(isObjectLike(undefined), false);
-    assert.strictEqual(isObjectLike(true), false);
-    assert.strictEqual(isObjectLike(Symbol("1")), false);
-    assert.strictEqual(isObjectLike(BigInt(1)), false);
-    assert.strictEqual(
-      isObjectLike(function () {}),
-      false
-    );
+    var values = falsey.concat(true, lodashStable, slice, 1, "a", symbol),
+      expected = lodashStable.map(values, stubFalse);
+    var actual = lodashStable.map(values, function (value, index) {
+      return index ? isObjectLike(value) : isObjectLike();
+    });
+    assert.deepStrictEqual(actual, expected);
+  });
+
+  it("should work with objects from another realm", function () {
+    if (realm.object) {
+      assert.strictEqual(isObjectLike(realm.boolean), true);
+      assert.strictEqual(isObjectLike(realm.date), true);
+      assert.strictEqual(isObjectLike(realm.number), true);
+      assert.strictEqual(isObjectLike(realm.object), true);
+      assert.strictEqual(isObjectLike(realm.regexp), true);
+      assert.strictEqual(isObjectLike(realm.string), true);
+    }
   });
 });
--- _lodash/test/util.js
+++ _lodash/test/util.js
@@ -2,7 +2,7 @@
  * @Author: 王晓婷 wangxiaoting@4paradigm.com
  * @Date: 2023-07-18 15:34:34
  * @LastEditors: 王晓婷 wangxiaoting@4paradigm.com
- * @LastEditTime: 2023-07-19 14:41:40
+ * @LastEditTime: 2023-07-24 17:53:05
  * @Description: test 工具函数
  */
 function toArgs(array) {
@@ -11,8 +11,25 @@ function toArgs(array) {
   }.apply(undefined, array);
 }
 
-var args = toArgs([1, 2, 3]);
+var arrayProto = Array.prototype,
+  root = (typeof global === "object" && global) || this;
 
-var falsey = [, null, undefined, false, 0, NaN, ''];
+var slice = arrayProto.slice;
 
-export { toArgs, args, falsey };
+var args = toArgs([1, 2, 3]),
+  realm = {};
+
+var falsey = [, null, undefined, false, 0, NaN, ""];
+
+var stubFalse = function () {
+    return false;
+  },
+  stubTrue = function () {
+    return true;
+  };
+
+var Symbol = root.Symbol;
+
+var symbol = Symbol ? Symbol("a") : undefined;
+
+export { toArgs, args, falsey, stubFalse, stubTrue, slice, symbol, realm };
