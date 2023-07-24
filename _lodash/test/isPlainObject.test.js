307f0e11dc63eb12b5a4b329bf282d3b52e1d20d
王晓婷 <wangxiaoting@4paradigm.com>
2023-07-24 19:02:34 +0800

    feat:isPlainObject

---
+++ _lodash/test/isPlainObject.test.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: 王晓婷 wangxiaoting@4paradigm.com
+ * @Date: 2023-07-24 17:59:18
+ * @LastEditors: 王晓婷 wangxiaoting@4paradigm.com
+ * @LastEditTime: 2023-07-24 17:59:41
+ */
+import assert from "assert";
+import stableLodash from "lodash";
+
+describe('isPlainObject', function(){
+  
+})
