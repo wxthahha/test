df5cedeb610ca89b7354e973e160428176c78aed
wangxiaoting <wangxiaoting@4paradigm.com>
2023-08-29 19:29:59 +0800

    feat:密码正则

--- regexp/src/index.js
+++ regexp/src/index.js
@@ -1,5 +1,10 @@
 const formatThousands = (string) => {
-  return `${string}`.replace(/(?!^)(?=(\d{3})+$)/g, ",");
+  return `${string}`.replace(/\B(?=(\d{3})+\b)/g, ",");
 };
 
-export { formatThousands };
+const format1 = (number) => {
+  // 1888 => $ 1,888.00
+  return parseFloat(number).toFixed(2).replace(/\B(?=(\d{3})+\b)/g, ",").replace(/^/, "$ ");
+};
+
+export { formatThousands, format1 };
--- regexp/test/index.test.js
+++ regexp/test/index.test.js
@@ -1,5 +1,5 @@
 import assert from "assert";
-import { formatThousands } from "../src/index";
+import { formatThousands, format1 } from "../src/index";
 
 describe("regexp", function () {
   it("千分位分隔整数", function () {
@@ -9,7 +9,10 @@ describe("regexp", function () {
   });
   // it("千分位分隔小数", function () {
   //   assert.strictEqual(formatThousands(123.12), "123.12");
-  //   assert.strictEqual(formatThousands(1234.124), "1,234.123");
+  //   assert.strictEqual(formatThousands(1234.123), "1,234.123");
   //   assert.strictEqual(formatThousands(1234.1234), "1,234.123,4");
   // });
+  it("货币格式化", function () {
+    assert.strictEqual(format1(1888), "$ 1,888.00");
+  });
 });
