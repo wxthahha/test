f2b4ac8d72479177eb70cd6ba05587b6e036e758
wangxiaoting <wangxiaoting@4paradigm.com>
2023-10-22 23:00:18 +0800

    docs:正则

--- 路白/promise/resolvePromise.test.js
+++
@@ -1,8 +0,0 @@
-// typeError
-// const promise2 = new Promise((resolve) => resolve(1)).then((value) => {
-//   return promise2;
-// });
-
-const promise2 = new Promise((resolve) => resolve(1)).then((value) => {
-  return new Promise(() => {});
-});
